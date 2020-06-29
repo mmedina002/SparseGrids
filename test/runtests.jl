2afa772f03b95a25d0cc7a7efcc3a28a4dcdb35e
Michelle Medina <mmedina002@drury.edu>
2020-06-28 22:19:58 -0500

    Added hierarchical basis function

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,3 +12,17 @@ x = [1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0]
 u=sin.(x)
 
 @test interpolate(x,u,1.5)==0.8753842058167891
+
+#Testing Hierarchical Basis Function
+x = collect(range(0, stop=1, length = 10001))
+
+plot(x, basis.(1, 1, x))
+plot(x, basis.(2, 1, x), "g")
+plot(x, basis.(2, 3, x), "g")
+plot(x, basis.(3, 1, x), "m")
+plot(x, basis.(3, 3, x), "m")
+plot(x, basis.(3, 5, x), "m")
+plot(x, basis.(3, 7, x), "m")
+
+xlabel("x")
+ylabel("phi(x)")
